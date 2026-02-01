7b95a3f8115bae80cba5596406f161b2a6840a11
taufiq-ai <taufiqkhantusar@gmail.com>
2026-02-01 16:59:16 +0600

    site: srp local data fetch

--- src/app/supabasedb.py
+++ src/app/supabasedb.py
@@ -33,13 +33,19 @@ def get_online_db_table(table_name: str, order_by: str=None, desc: bool=True):
         return response.data
     except Exception as e:
         logger.error(f"[Supabase Exception] Table: {table_name} - Exc: {str(e)}")
-        # FIXME: This is a temp solution. We should populate dev db during setup instead.
-        try:
-            logger.warning(f"[Local Data] Fetching from CSV: {table_name}")
-            return get_rows_from_csv(f".data/{table_name}.csv") if not settings.PRODUCTION else []
-        except Exception as csv_exc:
-            logger.error(f"[Local Data Exception] Table: {table_name} - Exc: {str(csv_exc)}")
-        return []
+        local_data = get_local_csv_data(table_name)
+        return local_data
 
+
+# FIXME: This is a temp solution. We should populate dev db during setup instead.
+def get_local_csv_data(table_name:str):
+    if settings.PRODUCTION:
+        return []
+    try:
+        logger.warning(f"[Local Data] Fetching from CSV: {table_name}")
+        return  get_rows_from_csv(f".data/{table_name}.csv")
+    except Exception as csv_exc:
+        logger.error(f"[Local Data Exception] Table: {table_name} - Exc: {str(csv_exc)}")
+        return []
 
 
